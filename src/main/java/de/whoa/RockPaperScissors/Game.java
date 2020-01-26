458f58204c216b97be2113e15155e760d5e6d837
robgg <rob@whoa.de>
2020-01-26 17:39:08 +0100

    Fill in class Game

--- src/main/java/de/whoa/RockPaperScissors/Game.java
+++ src/main/java/de/whoa/RockPaperScissors/Game.java
@@ -1,16 +1,29 @@
 package de.whoa.RockPaperScissors;
 
 import lombok.Getter;
+import lombok.ToString;
 
+@ToString
 public class Game {
+
+    @Getter
+    private Bet playerABet;
+
+    @Getter
+    private Bet playerBBet;
 
     @Getter
     private Outcome outcome;
 
     public Game(Bet a, Bet b) {
+        playerABet = a;
+        playerBBet = b;
     }
 
-    void eval(){
+    public void eval() {
+        outcome = !(playerABet.beats(playerBBet) || playerBBet.beats(playerABet)) ? Outcome.DRAW
+                : (playerABet.beats(playerBBet) ? Outcome.AWINS : Outcome.BWINS);
+        System.out.println(toString());
     }
 
 }
